2bc26d44e1050565e3b67ae66ddca5aa474a519a
JulesBrable <jules.brable7@gmail.com>
2024-04-14 15:10:07 +0000

    :sparkles: model evaluation

--- src/evaluation.py
+++ src/evaluation.py
@@ -1,18 +1,34 @@
 import matplotlib.pyplot as plt
 import torch
 from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay
+from sklearn.metrics import classification_report
+from typing import Union
+import os
+import json
 
 
-def plot_losses(train_losses, val_losses):
+def plot_losses(
+    train_losses,
+    val_losses,
+    stop: Union[int, None] = None,
+    save_path: Union[str, None] = None,
+    save_prefix: str = os.path.join("assets", "plots", "losses")
+):
+    if not os.path.exists(save_prefix):
+        os.makedirs(save_prefix)
+
     fig, ax = plt.subplots(figsize=(10, 5))
+    if stop:
+        ax.axvline(stop, linestyle='--', color='r', label='Early stopping')
     ax.plot(train_losses, label='Training Loss')
     ax.plot(val_losses, label='Validation Loss')
-    ax.set_title('Training and Validation Losses')
+    ax.set_title(f'Losses for {save_path.split("/")[-1]}')
     ax.set_xlabel('Epochs')
     ax.set_ylabel('Loss')
     ax.legend()
     ax.grid(True)
-    plt.show()
+    plt.savefig(os.path.join(save_prefix, save_path) + '.png')
+    return fig
 
 
 def predict(model, dataloader, device):
@@ -34,10 +50,27 @@ def predict(model, dataloader, device):
     return true_labels, predictions
 
 
-def plot_confusion_matrix(true_labels, predictions):
+def plot_confusion_matrix(
+    true_labels,
+    predictions,
+    save_path: Union[str, None] = None,
+    save_prefix: str = os.path.join("assets", "plots", "confusion"),
+):
+    if not os.path.exists(save_prefix):
+        os.makedirs(save_prefix)
+
     cm = confusion_matrix(true_labels, predictions)
     fig, ax = plt.subplots(figsize=(8, 6))
     disp = ConfusionMatrixDisplay(confusion_matrix=cm)
     disp.plot(ax=ax, cmap=plt.cm.Blues)
-    ax.set_title('Confusion Matrix')
-    plt.show()
+    ax.set_title(f'Confusion Matrix for {save_path.split("/")[-1]}')
+
+    if save_path:
+        plt.savefig(os.path.join(save_prefix, save_path) + '.png')
+    return fig
+
+
+def make_classif_report(y_true, y_pred, filepath):
+    report = classification_report(y_true, y_pred, output_dict=True)
+    with open(filepath, 'w') as file:
+        json.dump(report, file, indent=4)
