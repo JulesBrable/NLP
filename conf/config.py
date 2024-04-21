5938caaa0bff9aaccf80b3e0cf8653cb82941b9f
JulesBrable <jules.brable7@gmail.com>
2024-04-21 12:49:31 +0000

    delete flaubert

--- conf/config.py
+++ conf/config.py
@@ -1,10 +1,7 @@
 from transformers import BertTokenizer, BertForSequenceClassification
 from transformers import CamembertTokenizer, CamembertForSequenceClassification
-from transformers import FlaubertTokenizer, FlaubertForSequenceClassification
 
 models = {
-    'flaubert/flaubert_base_cased': [FlaubertTokenizer, FlaubertForSequenceClassification],
-    'flaubert/flaubert_base_uncased': [FlaubertTokenizer, FlaubertForSequenceClassification],
     'almanach/camembert-base': [CamembertTokenizer, CamembertForSequenceClassification],
     'almanach/camembert-large': [CamembertTokenizer, CamembertForSequenceClassification],
     'google-bert/bert-base-cased': [BertTokenizer, BertForSequenceClassification],
